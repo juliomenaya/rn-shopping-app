dd1c6a4ac0f8f6ddcb9e4c22c71dcd47bbaed0c6
juliomenaya <juliomenaya@gmail.com>
2021-02-22 22:06:52 -0600

    styling product item

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -37,7 +37,7 @@ const PRODUCTS = [
     'p5',
     'u3',
     'PowerBook',
-    'https://get.pxhere.com/photo/laptop-computer-macbook-mac-screen-water-board-keyboard-technology-air-mouse-photo-airport-aircraft-tablet-aviation-office-black-monitor-keys-graphic-hardware-image-pc-exhibition-multimedia-calculator-vector-water-cooling-floppy-disk-phased-out-desktop-computer-netbook-personal-computer-computer-monitor-electronic-device-computer-hardware-display-device-448748.jpg',
+    'https://image.freepik.com/free-photo/laptop-office-desk_426-19315253.jpg',
     'Awesome hardware, crappy keyboard and a hefty price. Buy now before a new one is released!',
     2299.99
   ),
