a74d0711cfdbf6f3fd6dc3d2b7b2a6b735d47d05
juliomenaya <juliomenaya@gmail.com>
2021-02-23 20:18:57 -0600

    reaching product detail and styling it

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -2,10 +2,12 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { Platform } from 'react-native';
 import ProductsOverview from '../screens/shop/ProductsOverview';
+import ProductDetail from '../screens/shop/ProductDetail';
 import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator({
-    ProductsOverview: ProductsOverview
+    ProductsOverview: ProductsOverview,
+    ProductDetail: ProductDetail
 }, {
     defaultNavigationOptions: {
         headerStyle: {
--- screens/shop/ProductsOverview.js
+++ screens/shop/ProductsOverview.js
@@ -14,9 +14,11 @@ const ProductsOverview = props => {
                 return (
                     <ProductItem 
                         image={itemData.item.imageUrl} 
-                        title={itemData.item.title} 
-                        price={itemData.item.price} 
-                        onViewDetail={() => {}} 
+                        title={itemData.item.title}
+                        price={itemData.item.price}
+                        onViewDetail={() => {
+                            props.navigation.navigate('ProductDetail', { productId: itemData.item.id, productTitle:  itemData.item.title });
+                        }} 
                         onAddToCart={() => {}} 
                     />
                 )
